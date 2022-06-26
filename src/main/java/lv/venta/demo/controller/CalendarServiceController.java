5088274e5bfe4d6ca345a76b21956ea91aa1fcdb
MafeleW <s21naglmada@venta.lv>
2022-06-26 17:07:13 +0300

    Added Calendar Service Impl

---
+++ src/main/java/lv/venta/demo/controller/CalendarServiceController.java
@@ -0,0 +1,9 @@
+package lv.venta.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CalendarServiceController {
+
+	
+}
--- src/main/java/lv/venta/demo/service/iCalendarService.java
+++ src/main/java/lv/venta/demo/service/iCalendarService.java
@@ -1,5 +1,17 @@
 package lv.venta.demo.service;
 
+import java.util.ArrayList;
+
+import lv.venta.demo.models.Calendar;
+import lv.venta.demo.models.Course;
+
 public interface iCalendarService {
 
+	public abstract boolean insertNewCalendarCourse(Calendar calendar);
+	
+	public abstract boolean UpdateCalendarCourseById(Calendar calendar, int id);
+	
+	public abstract boolean DeleteCalendarCourseById(int id);
+	
+	public ArrayList<Calendar> selectAllCal();
 }
